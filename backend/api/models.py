67b6c63ae6a40d71e0137659c25d075f47cc7e20
Ceejay Santos <7078ceejay@gmail.com>
2026-05-20 23:01:25 +0800

    refactor: move Admin model to user module and register in admin

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,15 +4,4 @@ from .user.models import *
 from django.contrib.auth.models import User
 #import the models here in nested folder to migrate
 
-class Admin(models.Model):
-    department_choices = [
-        ('TODA', 'Toda'),
-        ('MDRRMO', 'MDRRMO'),
-    ]
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100, choices=department_choices)
 
-
-    def __str__(self):
-        return self.user.username
