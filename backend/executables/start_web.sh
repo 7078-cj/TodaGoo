32cd257e8f79d488a83a9d6612ccf82be49430a8
Ceejay Santos <7078ceejay@gmail.com>
2026-05-22 03:51:37 +0800

    fix: remove seed.py execution from start_web.sh

--- backend/executables/start_web.sh
+++ backend/executables/start_web.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Start Django server
+python manage.py makemigrations 
 python manage.py migrate
 python manage.py makemigrations api
 python manage.py migrate api
-python seed.py
 python manage.py runserver 0.0.0.0:8000
