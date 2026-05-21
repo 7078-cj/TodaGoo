22b67086af582ad588923a68ed97cfb2939e2729
Ceejay Santos <7078ceejay@gmail.com>
2026-05-22 04:06:44 +0800

    fix: adjust spacing in start_web.sh for consistency

--- backend/executables/start_web.sh
+++ backend/executables/start_web.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Start Django server
-python manage.py makemigrations 
-python manage.py migrate
+python manage.py makemigrations
 python manage.py makemigrations api
 python manage.py migrate
+
 python manage.py runserver 0.0.0.0:8000
