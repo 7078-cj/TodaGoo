6f73f12111ce1d36fc10d8c42e8c02da3cf736b7
Ceejay Santos <7078ceejay@gmail.com>
2026-06-04 00:28:22 +0800

    added logo and show password

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 import { loginUser } from '../utils/auth';
+import TodaGoo from '../assets/TodaGoo.svg'
+import TodaGooLogo from '../assets/TodaGoo_Logo.svg'
+import { EyeClosed, EyeIcon } from 'lucide-react';
 
 function Login() {
   const dispatch = useDispatch()
   const nav = useNavigate()
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     if (!error) return;
@@ -25,14 +29,21 @@ function Login() {
     <div className="grid grid-cols-[60%_40%] min-h-screen bg-gray-100">
 
       <div className="bg-gradient-to-b from-[#a8c3b0] via-[#d9c9a8] to-[#e8c39b]">
-
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="w-full max-w-lg">
+            <img src={TodaGooLogo} alt="TODA GOO LOGO" className="w-full mx-auto mt-20" />
+            <img src={TodaGoo} alt="TODA GOO" className="w-full" />
+          </div>
+          
+        </div>
+        
       </div>
       
 
       {/* login form */}
       <div className="p-4 flex items-center justify-center flex-1">
         <div className="w-full max-w-[75%] ">
-          <h2 className="text-6xl font-bold mb-6 text-center text-gray-800">Welcome</h2>
+          <h2 className="text-6xl font-semibold mb-6 text-center text-gray-800">Welcome</h2>
 
           {error && (
             <p className="text-red-500 text-center mb-4">
@@ -56,12 +67,22 @@ function Login() {
 
             <label className="flex flex-col text-gray-700 font-medium">
               Password
-              <input
-                type="password"
-                name="password"
-                className={inputStyle}
-                placeholder="password"
-              />
+              <div className={`flex flex-row justify-between items-center ${inputStyle}`}>
+                
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  className="flex-1 bg-transparent outline-none w-full"
+                  placeholder="password"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="text-sm text-green-600 mt-1 self-end"
+                >
+                  {showPassword ? <EyeIcon /> : <EyeClosed />}
+                </button>
+              </div>
             </label>
 
             <div className="flex justify-center mt-6">
