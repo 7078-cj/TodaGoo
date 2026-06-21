95fcacd9477f999b80ae51f19c706d0927f93fd9
7078-cj <7078ceejay@gmail.com>
2026-06-21 22:21:15 +0800

    feat: define URL patterns for passenger list and detail views

--- backend/api/features/passenger/urls.py
+++ backend/api/features/passenger/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import (
+    PassengerListCreateView,
+    PassengerRetrieveUpdateDestroyView
+)
 
 urlpatterns = [
-    # Add your paths here
+    path('', PassengerListCreateView.as_view(), name="list_create_passenger"),
+    path('<int:pk>', PassengerRetrieveUpdateDestroyView.as_view(), name="retrieve_update_delete_passenger"),
 ]
