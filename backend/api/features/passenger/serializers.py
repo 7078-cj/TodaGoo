5df7bdfbec870ab826c38a136d4e089c34b5d52f
7078-cj <7078ceejay@gmail.com>
2026-06-22 22:46:06 +0800

    feat: include first name and last name in passenger serializer fields and creation

--- backend/api/features/passenger/serializers.py
+++ backend/api/features/passenger/serializers.py
@@ -24,6 +24,8 @@ class PassengerSerializer(serializers.ModelSerializer):
             'id',
             'username',
             'email',
+            'first_name',
+            'last_name',
             'password',
             'passenger_profile',
         )
@@ -37,6 +39,8 @@ class PassengerSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
         )
 
         Passenger.objects.create(
