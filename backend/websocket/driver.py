63d7f12641a61952103173f30a7f7e44dc5c51dd
7078-cj <7078ceejay@gmail.com>
2026-07-14 01:45:33 +0800

    feat: implement booking timeout logic and handle expired bookings in driver consumer

--- backend/websocket/driver.py
+++ backend/websocket/driver.py
@@ -8,6 +8,7 @@ from ..api.features.booking.models import Booking
 from ..api.features.booking.utils import assign_driver_sync
 from ..api.features.booking.models import DriverQueue
 from django.contrib.gis.geos import Point
+import asyncio
 
 
 
@@ -53,10 +54,43 @@ class DriverConsumer(AsyncWebsocketConsumer):
             }))
 
     async def new_booking(self, event):
+        booking_id = event["data"]["id"]
+
+        asyncio.create_task(self.booking_timeout(booking_id))
+
         await self.send(text_data=json.dumps({
             "type": "new_booking",
-            "data": event["data"]
+            "data": event["data"],
         }))
+
+    async def booking_timeout(self, booking_id):
+        await asyncio.sleep(120)
+
+        await self.booking_expired(booking_id)
+
+    @database_sync_to_async
+    def booking_expired(self, booking_id):
+        try:
+            booking = Booking.objects.get(id=booking_id)
+        except Booking.DoesNotExist:
+            return
+
+        if booking.status != Booking.Status.PENDING or booking.driver_id is not None:
+            return
+
+        driver = self.user.driver
+
+        DriverQueue.objects.get_or_create(
+            driver=driver,
+            defaults={
+                "location": driver.current_location,   
+            },
+        )
+
+        assign_driver_sync(
+            booking.id,
+            exclude_driver_ids=[driver.id],
+        )
 
     @database_sync_to_async
     def accept_booking(self, booking_id):
