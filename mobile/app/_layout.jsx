0f6950c6c5ee32eaecef818acb68d9c01f886fb0
7078-cj <7078ceejay@gmail.com>
2026-07-14 10:24:59 +0800

    refactor: update authentication context usage in RootNavigator

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,17 +1,19 @@
 import { Stack, router } from "expo-router";
 import "../global.css";
-import { AuthProvider, useAuth } from "../contexts/AuthContext";
-import { useEffect } from "react";
+import AuthContext from '@/contexts/AuthContext';
+import { AuthProvider } from "@/contexts/AuthContext";
+import { useContext, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { updateToken } from "@/api/auth";
 
 function RootNavigator() {
-  const { setToken, setUser, logoutUser } = useAuth();
+  const { setToken, setUser, logoutUser } = useContext(AuthContext);
 
   useEffect(() => {
     const checkAuth = async () => {
 
       const user = await AsyncStorage.getItem("user");
+      const parsedUser = user ? JSON.parse(user) : null;
 
       if (!user) {
           router.replace("/(global)/login");
@@ -20,7 +22,6 @@ function RootNavigator() {
 
       await updateToken(setToken, setUser, logoutUser);
 
-      const parsedUser = user ? JSON.parse(user) : null;
 
       if (parsedUser) {
         if (parsedUser.role === "passenger") {
